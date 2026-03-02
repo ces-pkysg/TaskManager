b3d7f61dd627f932f8d3e86b5b8c070940f5896d
ces-pkysg <paraaps991@gmail.com>
2026-03-02 16:40:19 -0600

    Boton Busca(Ajax) falta buscar por completada y pendientes

--- TaskManager.Web/wwwroot/js/Task/task-modal.js
+++ TaskManager.Web/wwwroot/js/Task/task-modal.js
@@ -1,7 +1,40 @@
 ﻿document.addEventListener("DOMContentLoaded", () => {
 
+    const btnBuscar = document.getElementById("btnBuscarAjax");//boton buscar Ajax
+    const formulario = document.getElementById("filterForm");//formulario de filtros
     const modal = new bootstrap.Modal(document.getElementById("taskModal"));
     const modalContent = document.getElementById("taskModalContent");
+    const contenedor = document.getElementById("taskTableContainer");//donde está la tabla
+
+
+    btnBuscar.addEventListener("click", async () => {
+
+        // Convertimos el formulario en QueryString automático
+        const formData = new FormData(formulario);
+        const query = new URLSearchParams();
+
+        formData.forEach((value, key) => {
+            if (value !== null && value !== "") {
+                query.append(key, value);
+            }
+        });
+
+        // Construimos la URL para el GET parcial
+        let url = '/Tasks/LoadTablePartial?' + query.toString();
+
+        // Llamada AJAX
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            contenedor.innerHTML = "<p>Error al cargar resultados.</p>";
+            return;
+        }
+
+        const html = await response.text();
+
+        // Reemplazamos la tabla
+        contenedor.innerHTML = html;
+    });
 
     // CREAR
     document.getElementById("btnCrearTask")
