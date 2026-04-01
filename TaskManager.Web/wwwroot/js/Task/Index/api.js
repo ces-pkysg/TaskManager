2cdaa4d69fad076c0317da819bbba8695fa96b94
ces-pkysg <paraaps991@gmail.com>
2026-04-01 15:33:24 -0600

    Correccion de urls y referencias en core.js, nuevos endpoints en api.js

--- TaskManager.Web/wwwroot/js/Task/Index/api.js
+++ TaskManager.Web/wwwroot/js/Task/Index/api.js
@@ -3,7 +3,11 @@
     const ns = window.Tasks.index = window.Tasks.index || {};
     ns.api = ns.api || {};
 
+
+    //aqui solo van endpoints
+
     ns.api = {
+        //obtener formulario para crear
         crearPartial: function () {
             return $.ajax({
                 url: ns.core.urls.createPartial,
@@ -11,15 +15,15 @@
             });
         },
 
-
-         editarPartial: function (id) {
+        //obtener formulario para editar
+        editarPartial: function (id) {
             return $.ajax({
                 url: ns.core.urls.editPartial + id,
                 type: 'GET'
             });
-         },
+        },
 
-
+        //guardar tarea
         guardarTarea: function (data) {
             return $.ajax({
                 url: ns.core.urls.create,
@@ -28,6 +32,7 @@
                 contentType: 'application/json'
             });
         },
+
 
         guardarEdicionTarea: function (data, id) {
             return $.ajax({
@@ -38,6 +43,7 @@
             });
         },
 
+
         eliminarTarea: function (id) {
             return $.ajax({
                 url: ns.core.urls.delete + id,
@@ -46,45 +52,25 @@
             });
         },
 
-        cargarTareas: function (id) {
+
+        cargarTareas: function (query) {
             return $.ajax({
                 url: ns.core.urls.loadTablePartial + query.toString(),
                 type: 'GET',
 
             });
+        },
+
+
+        //obtener categorias
+        obtenerCategorias: function () {
+            return $.ajax({
+                url: ns.core.urls.getCategories,
+                type: 'GET',
+
+            });
         }
     };
-
-    //ns.api.validaEstatusTV429 = async function () {
-    //    loading.show();
-
-    //    ns.datosValidaEstatusTV4 = null;
-
-    //    model = {
-            
-    //    };
-
-    //    return $.ajax({
-    //        url: ns.urls.validaEstatusTV429Evento,
-    //        type: 'POST',
-    //        data: model
-    //    }).then(async data => {
-    //        if (data.success) {
-    //            ns.datosValidaEstatusTV4 = data.data;
-    //            loading.hide();
-    //            return true;
-    //        } else {
-    //            notificacion.warning("No se pudo validar el estatus de la TV.");
-    //        }
-
-    //        loading.hide();
-    //        return false;
-    //    }).catch(async error => {
-    //        notificacion.error("Ocurrió un error al verificar el estatus de la TV.");
-    //        loading.hide();
-    //        return false;
-    //    });
-    //};
 
 
 })(window, jQuery);
@@ -97,3 +83,37 @@
 //        data: payload
 //    });
 //}
+
+
+
+
+//ns.api.validaEstatusTV429 = async function () {
+//    loading.show();
+
+//    ns.datosValidaEstatusTV4 = null;
+
+//    model = {
+
+//    };
+
+//    return $.ajax({
+//        url: ns.urls.validaEstatusTV429Evento,
+//        type: 'POST',
+//        data: model
+//    }).then(async data => {
+//        if (data.success) {
+//            ns.datosValidaEstatusTV4 = data.data;
+//            loading.hide();
+//            return true;
+//        } else {
+//            notificacion.warning("No se pudo validar el estatus de la TV.");
+//        }
+
+//        loading.hide();
+//        return false;
+//    }).catch(async error => {
+//        notificacion.error("Ocurrió un error al verificar el estatus de la TV.");
+//        loading.hide();
+//        return false;
+//    });
+//};
--- TaskManager.Web/wwwroot/js/Task/Index/core.js
+++ TaskManager.Web/wwwroot/js/Task/Index/core.js
@@ -14,18 +14,18 @@
     ns.core.$btnBuscar = $("#btnBuscarAjax");
     ns.core.$btnCreateTask = $("#btnCrearTask");
     ns.core.$btnSaveTask = $("#btnSaveTask");
-    ns.core.$btnEditTask = $(".btnEdit"); //??????
-    ns.core.$btnDeleteTask = $(".btnDelete");//??????
+    ns.core.$btnEditTask = $(".btnEdit"); //se quita # porque esta como una clase (.)
+    ns.core.$btnDeleteTask = $(".btnDelete");//se quita # porque esta como una clase (.)
 
 
     //URLs (rutas del modulo)
     ns.core.urls = {
         createPartial: '/Task/CreatePartial',
-        editPartial: '/Tasks/EditPartial/' + id,
-        delete: '/Tasks/DeleteAjax/' + id,
-        edit: '/Tasks/Edit/' + data.Id,
+        editPartial: '/Tasks/EditPartial/',//el + id se concatena cuando se use,
+        delete: '/Tasks/DeleteAjax/',// + id se concatena cuando se use,
+        edit: '/Tasks/Edit/', // el + data.Id se concatena cuando se use,,
         create: '/Tasks/Create',
-        loadTablePartial: '/Tasks/LoadTablePartial?' + query.toString(),
+        loadTablePartial: '/Tasks/LoadTablePartial?', // el + query.toString() se concatena cuando se use,
         getCategories: "/Categories/GetCategories"
     };
 
